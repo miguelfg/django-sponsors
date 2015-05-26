e556a643309e221c3958b9f7752d202931cc13e4
miguelfg <miguel.fiandor.gutierrez@gmail.com>
2015-05-26 20:16:28 +0200

    url fix

--- sponsors/urls.py
+++ sponsors/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^index/$', views.IndexView.as_view(), name='template-index'),
     url(r'^list/$', views.SponsorsList.as_view(), name='list-sponsors'),
 )
